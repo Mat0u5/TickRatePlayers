d029bf8c9a07c21185fbc08a80d29e384c67468a
Mat0u5 <matous.danko+github@gmail.com>
2025-07-20 11:56:04 +0200

    Finished.

---
+++ README.md
@@ -0,0 +1,4 @@
+This simple mod allows players to tick (so move, jump, attack, take damage) faster when using the vanilla tick rate command, just like other entities.<br>
+Because in vanilla, the player can only get slowed down by the tick rate command, not sped up.
+
+This mod also speeds up all sounds or slows them down based on the tick rate.
--- src/main/java/net/mat0u5/tickrateplayers/mixin/SoundSystemMixin.java
+++ src/main/java/net/mat0u5/tickrateplayers/mixin/SoundSystemMixin.java
@@ -3,7 +3,6 @@ package net.mat0u5.tickrateplayers.mixin;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.sound.SoundInstance;
 import net.minecraft.client.sound.SoundSystem;
-import net.minecraft.util.math.MathHelper;
 import net.minecraft.world.tick.TickManager;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -12,13 +11,15 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(value = SoundSystem.class)
 public class SoundSystemMixin {
-    @Inject(method = "getAdjustedPitch", at = @At("HEAD"), cancellable = true)
+    @Inject(method = "getAdjustedPitch", at = @At("RETURN"), cancellable = true)
     private void getAdjustedPitch(SoundInstance sound, CallbackInfoReturnable<Float> cir) {
         MinecraftClient client = MinecraftClient.getInstance();
         if (client.world != null) {
             TickManager tickManager = client.world.getTickManager();
             if (tickManager.getTickRate() != 20) {
-                cir.setReturnValue(MathHelper.clamp(sound.getPitch(), 0.5F, 2.0F) * (tickManager.getTickRate() / 20.0f));
+                Float original = cir.getReturnValue();
+                if (original == null) return;
+                cir.setReturnValue(original * (tickManager.getTickRate() / 20.0f));
             }
         }
     }
